9e410878f5e9492b0443f3321fae755195950073
Wichayaporn Wongkamjan <54638018+wwongkamjan@users.noreply.github.com>
2025-10-02 17:05:28 -0400

    update gpr_guesser (load tar.gz)

--- tfidf_guesser/gpr_guesser.py
+++ tfidf_guesser/gpr_guesser.py
@@ -4,6 +4,7 @@ from string import ascii_lowercase
 from math import floor
 from collections import defaultdict
 
+import tarfile
 import json
 from json import JSONDecodeError
 from tqdm import tqdm
@@ -14,10 +15,13 @@ import logging
 from baseconv import BaseConverter
 
 
-
 kCACHE_MISS = "CACHE_MISS"
 from nltk.corpus import stopwords
 from guesser import alphanum
+
+def clean_probs(log_probs):
+    probs = [y for x, y in log_probs]
+    return sum(probs) / len(probs)
 
 class GprGuesser(Guesser):
     """
@@ -26,7 +30,7 @@ class GprGuesser(Guesser):
     reproducability.
     """
 
-    def __init__(self, cache_filename="data/gpt3_cache.json", num_examples=2, num_shards=9999, shard_vocab=ascii_lowercase, shard_prefix_length=5):
+    def __init__(self, cache_filename, num_examples=2, num_shards=9999, shard_vocab=ascii_lowercase, shard_prefix_length=5, save_every=50):
         """
 
         @param num_examples: How many retrieval results to include in GPT prompt
@@ -34,6 +38,8 @@ class GprGuesser(Guesser):
         self.retrievers = {}
         self.cache = {}
         self.num_queries = 0
+        self.save_every = save_every
+        self.last_save = -1
         self.num_shards = num_shards
         self.num_examples = num_examples
         self.cache_filename = cache_filename
@@ -79,56 +85,87 @@ class GprGuesser(Guesser):
         """
         # Remove non-breaking spaces
         question = question.replace("\xa0", " ")
+
+        if self.num_queries % self.save_every == 0:
+            self.save()
         
         # Check the cache, return it from there if we have it
         if question not in self.cache:
             result = None
             while result is None:
                     result = kCACHE_MISS
-                    print("No cache found for: |%s|" % question)                                
+                    # logging.info("No cache found for: |%s|" % question)                                
                     
             if result != kCACHE_MISS:
                 self.cache[question] = result
 
         if question in self.cache:
-            return [self.cache[question]]
+            return [{"guess": self.cache[question]["guess"],
+                     "confidence": clean_probs(self.cache[question]["confidence"])}]
         else:  # If we get here, this means that we couldn't query GPT and it's not cached
             assert result == kCACHE_MISS
             return [{"guess": "", "confidence": 0.0}]
-        
-        
-    def save(self, suffix=""):
+
+    def save(self, cache_filename=None, queries=None, suffix=".json"):
         """
         Save the API results to a file to save money and time for the future
         """
 
-        if self.num_queries > 0:
-            logging.info("Made %i new queries, saving to %s" % (self.num_queries, self.cache_filename))
+        force_write = cache_filename is not None and queries is not None
+
+        if cache_filename is None:
+            cache_filename=self.cache_filename
+        if queries is None:
+            queries=self.cache
+        
+        # Save if we have something to save and we haven't already saved it
+        # alternately, if we have specified a different filename, always save
+        if (self.num_queries > 0 and self.last_save != self.num_queries) or force_write:
+            logging.info("Made %i new queries, saving to %s [cf: %s / queries: %s]" % (self.num_queries, cache_filename, cache_filename, str(queries is None)))
             shards = defaultdict(dict)
-            for ii in self.cache:
+            for ii in queries:
                 shard = self.shard(ii)
                 shards[shard][ii] = self.cache[ii]
 
             for shard in tqdm(shards):
-                filename = "%s%05i%s" % (self.cache_filename, shard, suffix)
+                filename = "%s%05i%s" % (cache_filename, shard, suffix)
                 with open(filename, 'w') as outfile:
                     json_object = json.dumps(shards[shard], indent=2)
-                    outfile.write(json_object)            
+                    outfile.write(json_object)
+
+            tar = tarfile.open("%s.tar.gz" % cache_filename, 'w:gz')
+            for shard in tqdm(shards):
+                filename = "%s%05i%s" % (cache_filename, shard, suffix)
+                tar.add(filename)
+            tar.close()
+
+        self.last_save = self.num_queries
         
-
-
 
             
     def load(self):
         """
         Load the cache of search results from a file
         """
-        from glob import glob
-        self.cache = {}
-        for ii in glob("%s*" % self.cache_filename):
+
+        clean = {}
+
+        try:
+            tar = tarfile.open("%s.tar.gz" % self.cache_filename, 'r:gz')
+        except tarfile.ReadError:
+            logging.debug("Empty cache, creating empty")
+            return self.cache
+            
+        for ii in tar.getmembers():
+            if ii.name.endswith(".pkl") or "/._" in ii.name:
+                logging.debug("Skipping %s" % ii)
+                continue
+            else:
+                logging.debug("Reading from %s" % ii)
             try:
-                with open(ii, 'r') as infile:
-                    json_object = json.load(infile)
+                with tar.extractfile(ii) as infile:
+                    raw_text = infile.read().decode('utf-8', errors='ignore')
+                    json_object = json.loads(raw_text)
                     clean = {}
                     # Deal with non-breaking space issue
                     for ii in json_object:
@@ -136,42 +173,71 @@ class GprGuesser(Guesser):
             except (IOError, JSONDecodeError) as e:
                 json_object = {}
                 print("Failed to load cache")
+
             self.cache.update(json_object)
             self.cache.update(clean)
             logging.debug("Reading %09i entries from %s, cache size is now %09i" % (len(json_object), ii, len(self.cache)))
 
+        tar.close()
         logging.info("%i entries added to cache" % len(self.cache))
         return self.cache
 
 
 
-
+        prompt = [instructions] + prompt
     
 
 if __name__ == "__main__":
+    import argparse
     import gzip
     import logging
     from buzzer import runs
 
     logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source_jsongz', type=str, default="../data/qanta.buzztrain.json.gz")
+    parser.add_argument('--build_cache', action="store_true", help="Save cache", default=False)
+    parser.add_argument('--cache', type=str, default="../models/gpt_cache")
+    parser.add_argument('--cache_copy', type=str, default=None)
+    parser.add_argument('--run_length', type=int, default=100)
+    parser.add_argument('--limit', type=int, default=-1)
+    flags = parser.parse_args()
     
-    gg = GprGuesser(cache_filename="models/GprGuesser")
+    gg = GprGuesser(cache_filename=flags.cache)
     gg.load()
 
-    with gzip.open("data/qanta.buzztrain.json.gz") as infile:
+    with gzip.open(flags.source_jsongz) as infile:
         questions = json.load(infile)
 
+    print("Loaded %i question" % len(questions))
+        
     misses = 0
     hits = 0
-    for qq in questions:
-        for rr in runs(qq["text"], 100):
+    print("Generating runs of length %i" % flags.run_length)
+
+    if flags.limit > 0:
+        questions = questions[:flags.limit]
+
+    queries = set()
+    for qq in tqdm(questions):
+        for rr in runs(qq["text"], flags.run_length):
             hit = rr in gg.cache
             if hit:
+                queries.add(rr)
                 hits += 1
             else:
-                print(rr)
+                # print(rr)
+                if flags.build_cache:
+                    gg(rr)
                 misses += 1
 
+    gg.save()
+    print("---------------------")
     print(hits / (hits + misses))
 
-    
+    logging.info("Hit ratio: %f" % (hits / (hits + misses)))
+
+    if flags.cache_copy:
+        print("Saving to %s" % flags.cache_copy)
+        gg.save(cache_filename=flags.cache_copy, queries=queries)
